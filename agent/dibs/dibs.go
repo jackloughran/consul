a64970077aee05e0615833eba7fdc9cf1ece5b41
Jack Loughran <30052269+jackloughran@users.noreply.github.com>
2020-08-19 16:28:28 -0400

    fix up comment

--- agent/dibs/dibs.go
+++ agent/dibs/dibs.go
@@ -1,11 +1,10 @@
 // Package dibs contains most of the actual logic around our configs.
 // Most services will probably request configs by filename. First, they get a list of
 // each file name. Then, they make separate requests for each file name. Those separate
-// requests must be made with the config version from consul. This is for two reasons:
-// 1) We don't want the configs to change half way through these file name requests and
-// the service to get some config files from one version and some from another. 2) We
-// don't want to have to refetch and reprocess the configs every time because it's a
-// pretty slow process. We will cache the latest 3 versions of processed configs.
+// requests must be made with the config version from consul. We don't want the configs
+// to change half way through these file name requests and the service to get some
+// config files from one version and some from another. We will cache the latest 3
+// versions of processed configs in the calling package.
 package dibs
 
 import (
